9917bf9c020b0192b23a38e882f19ef96bc168cf
Nils ten Hoeve <nilstenhoeve@gmail.com>
2022-06-04 20:07:23 +0200

    Fixed #12

--- example/column_example_page.dart
+++ example/column_example_page.dart
@@ -13,14 +13,14 @@ class ColumnExamplePage extends StatelessWidget {
           color: Colors.yellow,
           padding: const EdgeInsets.all(8),
           child: Container(
-              color: Colors.red,
+              color: Colors.grey,
               child: ResponsiveLayoutGrid.builder(cellBuilder: _cellBuilder))));
 
   List<Widget> _cellBuilder(Layout layout) {
     List<Widget> children = [];
     for (int i = 0; i < layout.nrOfColumns; i++) {
       children.add(Container(
-        color: Colors.green,
+        color: Colors.blue[100],
         child: Center(
           child: Text("Column ${i + 1}"),
         ),
